ced3a7dfeb5d7054a938146190b77e72745bf5d0
Eric Ngo <quocdat32461997@yahoo.com>
2020-09-12 09:59:12 -0500

    Updates

--- AdvancedML/BiLSTM-CRF/src/model.py
+++ AdvancedML/BiLSTM-CRF/src/model.py
@@ -2,13 +2,40 @@
 output.py - module to store BiLSTM-CRF model
 """
 
+import numpy as np
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, LSTM, Embedding, TimeDistributed, Dropout, Bidirectional, Dense
+from tensorflow.keras.initializers import Constant
 from .loss import CRF
-def embedding_layer(input_dim, output_dim, input_length, mask_zero):
-    return Embedding(input_dim = input_dim, output_dim = output_dim, input_length = input_length, mask_zero = mask_zero)
-    
-def bilstm_crf(maxlen, n_tags, embedding_dim, n_words, mask_zero, training = True):
+
+def embedding_map(glove_path = 'glove.6B.200d.txt'):
+    """
+	    embedding_map - function to load weights of the pretrained Glove embedding file
+	    Parameters:
+		    glove_path              I/P     path to the pretrained Glove embedding file
+		    embedding_index		O/p	weights of the pretrained Glove embedding file
+	  """
+	  embeddings_index = {}
+	  with open(glove_path) as f:
+		  for line in f:
+		  	word, coefs = line.split(maxsplit = 1)
+		  	coefs = np.fromstring(coefs, 'f', sep = ' ')
+			  embeddings_index[word] = coefs
+	  print('Found %s word vectors.' %len(embeddings_index))
+	  return embeddings_index
+
+def embedding_layer(word2dix, input_dim, output_dim, input_length, mask_zero):
+    embedding_index = embedding_map()
+
+    embedding_matrix = np.zeros((input_dim, output_dim))
+    for word, i in word2dix.items():
+      embedding_vector = embdding_index.get(word)
+      if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+    return Embedding(input_dim = input_dim, output_dim = output_dim, input_length = input_length, trainable = False, embeddings_initializer = Constant(embedding_matrix))
+
+def bilstm_crf(word2dix, maxlen, n_tags, embedding_dim, n_words, mask_zero, training = True):
     """
     bilstm_crf - module to build BiLSTM-CRF model
     Inputs:
@@ -28,6 +55,6 @@ def bilstm_crf(maxlen, n_tags, embedding_dim, n_words, mask_zero, training = Tru
 
     # Dense layer
     output = TimeDistributed(Dense(n_tags, activation = 'relu'))(output)
-    
+
     output = CRF(n_tags, name = 'crf_layer')(output)
     return Model(input, output)
